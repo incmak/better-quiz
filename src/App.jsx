0ad247cd81214c53488cfe5fb3c1ffac79e981c4
incmak <ahmedmuieen@gmail.com>
2023-02-14 14:33:52 +0530

    feat: :tada: New implementation of scrimba quiz project

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "react";
+import Quiz from "./components/Quiz";
+function App() {
+	const [quizStarted, setQuizStarted] = useState(
+		localStorage.getItem("quizStarted") || false
+	);
+	const [totalScore, setTotalScore] = useState(
+		localStorage.getItem("totalScore") || 0
+	);
+	const [questions, setQuestions] = useState([]);
+	const [options, setOptions] = useState([]);
+	const [correctAnswer, setCorrectAnswer] = useState({});
+	const [responses, setResponses] = useState({});
+	const [score, setScore] = useState(0);
+	const [reset, setReset] = useState(false);
+	const [submit, setSubmit] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const baseURL =
+		"https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple";
+
+	const fetchQuestions = async () => {
+		setLoading(true);
+		const response = await fetch(baseURL);
+		const data = await response.json();
+		setQuestions(data.results);
+		setOptions(
+			data.results.map((question) => {
+				const options = [
+					...question.incorrect_answers,
+					question.correct_answer,
+				];
+				return options.sort(() => Math.random() - 0.5);
+			})
+		);
+		setCorrectAnswer(
+			data.results.reduce((acc, question, index) => {
+				acc[`Q${index + 1}`] = question.correct_answer;
+				return acc;
+			}, {})
+		);
+		setLoading(false);
+	};
+
+	const startQuiz = () => {
+		setLoading(true);
+		localStorage.setItem("quizStarted", true);
+		setQuizStarted(true);
+	};
+	const handleRadioChange = (e) => {
+		setResponses({ ...responses, [e.target.name]: e.target.value });
+	};
+	const submitQuiz = () => {
+		localStorage.removeItem("quizStarted");
+		setQuizStarted(false);
+	};
+	const checkAnswers = (e) => {
+		e.preventDefault();
+		setSubmit(true);
+		console.log(responses);
+		console.log(correctAnswer);
+		let Score = 0;
+		for (let key in responses) {
+			if (responses[key] === correctAnswer[key]) {
+				Score++;
+			}
+		}
+		setScore(Score);
+		setTotalScore(Number(totalScore) + Number(Score));
+		localStorage.setItem("totalScore", Score + Number(totalScore));
+	};
+	useEffect(() => {
+		if (quizStarted) {
+			fetchQuestions();
+		}
+	}, [quizStarted, reset]);
+	return (
+		<div className="App">
+			<img
+				src="/top-bubble.svg"
+				className="absolute top-0 right-0 z-[-1]"
+				alt=""
+			/>
+			{quizStarted ? (
+				<Quiz
+					handleRadioChange={handleRadioChange}
+					checkAnswers={checkAnswers}
+					submitQuiz={submitQuiz}
+					submit={submit}
+					setSubmit={setSubmit}
+					loading={loading}
+					score={score}
+					setScore={setScore}
+					setReset={setReset}
+					questions={questions}
+					options={options}
+					correctAnswer={correctAnswer}
+					responses={responses}
+					totalScore={totalScore}
+					setTotalScore={setTotalScore}
+				/>
+			) : (
+				<div className="flex h-screen flex-col items-center justify-center gap-5">
+					<h3 className="text-3xl font-bold text-[#293264]">Quizzical</h3>
+					<button
+						className="flex h-[52px] w-48 items-center justify-center rounded-2xl bg-[#4D5B9E] font-medium text-[#F5F7FB]"
+						onClick={startQuiz}
+					>
+						Start quiz
+					</button>
+				</div>
+			)}
+			<img
+				src="/bottom-bubble.svg"
+				className="absolute bottom-0 left-0 z-[-1]"
+				alt=""
+			/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import { decode } from "html-entities";
+
+function Quiz({
+	questions,
+	options,
+	correctAnswer,
+	handleRadioChange,
+	checkAnswers,
+	submitQuiz,
+	submit,
+	setSubmit,
+	loading,
+	score,
+	setScore,
+	setReset,
+	responses,
+	totalScore,
+	setTotalScore,
+}) {
+	return (
+		<div className="pt-10">
+			{loading ? (
+				<div className="loading-container">
+					<div className="spinner"></div>
+					<div className="spinner-center"></div>
+					<div className="loading-text">Loading...</div>
+				</div>
+			) : (
+				<>
+					<div className="absolute top-5 right-0 flex flex-col items-end justify-end gap-0.5 lg:right-10">
+						<button
+							className="rounded-md bg-[#293264] px-3 py-1 text-white"
+							onClick={submitQuiz}
+						>
+							Exit
+						</button>
+						{/* <button>
+							<img src="/refresh.svg" alt="" />
+						</button>
+						<button>
+							<img src="/share.svg" alt="" />
+						</button>
+						<button>
+							<img src="/info.svg" alt="" />
+						</button>
+						<button>
+							<img src="/settings.svg" alt="" />
+						</button>
+						<button>
+							<img src="/help.svg" alt="" />
+						</button> */}
+						<button
+							className="rounded-md bg-[#293264] px-3 py-1 text-white"
+							onClick={() => {
+								localStorage.setItem("totalScore", 0);
+								setTotalScore(0);
+							}}
+						>
+							Reset
+						</button>
+					</div>
+					<form onSubmit={checkAnswers}>
+						<ul>
+							{questions.map((question, index) => (
+								<div key={index}>
+									<li className="pb-4 font-bold text-[#293264]">
+										Q.{index + 1} {decode(question.question)}
+									</li>
+									<div className="flex flex-wrap items-center gap-3">
+										{options[index].map((option, i) => (
+											<li key={i}>
+												<input
+													type="radio"
+													name={`Q${index + 1}`}
+													value={option}
+													id={option + index}
+													onChange={handleRadioChange}
+													checked={responses[`Q${index + 1}`] === option}
+												/>
+												<label
+													htmlFor={option + index}
+													style={{
+														backgroundColor:
+															submit &&
+															correctAnswer[`Q${index + 1}`] === option
+																? "#94D7A2"
+																: "",
+														border:
+															submit &&
+															correctAnswer[`Q${index + 1}`] === option
+																? "none"
+																: "",
+													}}
+													className={` ${
+														submit ? "submitted opacity-60" : ""
+													} cursor-pointer rounded-lg border border-[#4D5B9E] px-4 py-1 font-medium`}
+												>
+													{decode(option)}
+												</label>
+											</li>
+										))}
+									</div>
+									<div className="mb-4 border-b border-b-[#DBDEF0] pt-4" />
+									{/* <input type="radio" name={index} value={options[index]} />
+										{options[index]} */}
+								</div>
+							))}
+						</ul>
+						<div className="flex w-full items-center justify-center">
+							{submit && (
+								<p className="pr-5 text-sm font-bold text-[#293264]">
+									You scored {score}/5 correct answers
+								</p>
+							)}
+							{!submit ? (
+								<button
+									className=" h-9 w-[120px] rounded-[10px] bg-[#4D5B9E] text-xs font-semibold text-[#F5F7FB]"
+									type="submit"
+									onClick={checkAnswers}
+								>
+									Check answers
+								</button>
+							) : (
+								<button
+									className=" h-9 w-[120px] rounded-[10px] bg-[#4D5B9E] text-xs font-semibold text-[#F5F7FB]"
+									type="button"
+									onClick={(e) => {
+										e.preventDefault();
+										e.stopPropagation();
+										setScore(0);
+										setReset((prev) => !prev);
+										setSubmit(false);
+									}}
+								>
+									Play Again
+								</button>
+							)}
+						</div>
+						<p className="flex justify-start font-semibold">
+							Total Score: {totalScore}
+						</p>
+					</form>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default Quiz;
