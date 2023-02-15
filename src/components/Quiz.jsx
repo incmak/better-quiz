06afdb470e428085a7b747111ebf7da59eca7fe5
incmak <ahmedmuieen@gmail.com>
2023-02-15 20:05:39 +0530

    add new icons and popup

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { decode } from "html-entities";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+import { BsGear, BsShare, BsInfoCircle } from "react-icons/bs";
+import { SlRefresh } from "react-icons/sl";
+import { MdLogout } from "react-icons/md";
+import { RxCrossCircled } from "react-icons/rx";
 
 function Quiz({
 	questions,
@@ -20,8 +25,35 @@ function Quiz({
 	setTotalScore,
 }) {
 	const [menu, showMenu] = useState(false);
+	const [showSettings, setShowSettings] = useState(false);
+	const [showShare, setShowShare] = useState(false);
 	return (
 		<div className="pt-10">
+			<div
+				style={{
+					display: showSettings ? "flex" : "none",
+				}}
+				className="fixed top-0 left-0 z-10 flex h-full w-full items-center justify-center bg-black bg-opacity-50"
+			>
+				<div className="relative flex flex-col gap-4 rounded-lg bg-white p-4">
+					<button onClick={() => setShowSettings(false)}>
+						<RxCrossCircled className="absolute top-1 right-1" />
+					</button>
+					<h3 className="text-2xl font-bold text-[#293264]">
+						Total Score: {totalScore}
+					</h3>
+					<button
+						className="rounded-md bg-red-500 px-3 py-1 text-white"
+						onClick={() => {
+							localStorage.setItem("totalScore", 0);
+							setTotalScore(0);
+						}}
+					>
+						Reset Progress
+					</button>
+				</div>
+			</div>
+
 			{loading ? (
 				<div className="loading-container">
 					<div className="spinner"></div>
@@ -34,47 +66,48 @@ function Quiz({
 						id="hamburger"
 						className={`hamburger ${
 							menu ? "active" : ""
-						} absolute top-4  right-4 md:hidden`}
+						} fixed top-4  right-4 md:hidden`}
 						onClick={() => showMenu((menu) => !menu)}
 					>
-						<div class="line"></div>
-						<div class="line"></div>
-						<div class="line"></div>
+						<div className="line"></div>
+						<div className="line"></div>
+						<div className="line"></div>
 					</button>
 					<div
 						className={`menu-items ${
 							menu ? "active" : ""
-						} fixed top-5 right-5 flex flex-col items-end justify-end gap-0.5 lg:right-10`}
+						} fixed top-5 right-5 flex flex-col items-end justify-end gap-4  rounded-lg bg-[#293264] p-4 md:bg-transparent lg:right-10`}
 					>
-						<button
-							className="rounded-md bg-[#293264] px-3 py-1 text-white"
-							onClick={submitQuiz}
-						>
-							Exit
+						<button onClick={submitQuiz}>
+							<MdLogout
+								className="font-bold text-white md:text-[#293264]"
+								size={26}
+							/>
 						</button>
-						{/* <button>
-							<img src="/refresh.svg" alt="" />
+						<button onClick={() => setShowSettings(true)}>
+							<BsGear
+								className="font-bold text-white md:text-[#293264]"
+								size={26}
+							/>
+						</button>
+
+						<button>
+							<SlRefresh
+								className="font-bold text-white md:text-[#293264]"
+								size={26}
+							/>
+						</button>
+						<button onClick={() => setShowShare(true)}>
+							<BsShare
+								className="font-bold text-white md:text-[#293264]"
+								size={26}
+							/>
 						</button>
 						<button>
-							<img src="/share.svg" alt="" />
-						</button>
-						<button>
-							<img src="/info.svg" alt="" />
-						</button>
-						<button>
-							<img src="/settings.svg" alt="" />
-						</button>
-						<button>
-							<img src="/help.svg" alt="" />
-						</button> */}
-						<button
-							className="rounded-md bg-[#293264] px-3 py-1 text-white"
-							onClick={() => {
-								localStorage.setItem("totalScore", 0);
-								setTotalScore(0);
-							}}
-						>
-							Reset
+							<BsInfoCircle
+								className="font-bold text-white md:text-[#293264]"
+								size={26}
+							/>
 						</button>
 					</div>
 					<form onSubmit={checkAnswers}>
@@ -162,6 +195,155 @@ function Quiz({
 					</form>
 				</>
 			)}
+			{/* Share  */}
+			<div
+				style={{
+					display: showShare ? "flex" : "none",
+				}}
+				className="fixed top-0 left-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50"
+			>
+				<div className="mx-4 w-full rounded-xl bg-gray-100 p-4 md:w-[400px]">
+					<div className="items center flex justify-between border-b border-gray-200 py-3">
+						<div className="flex items-center justify-center">
+							<p className="text-xl font-bold text-gray-800">
+								Share Better Quiz with your friends
+							</p>
+						</div>
+
+						<button
+							onClick={() => setShowShare(false)}
+							className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-gray-300 font-sans text-gray-500 hover:bg-gray-500 hover:text-gray-300"
+						>
+							x
+						</button>
+					</div>
+
+					<div className="my-4">
+						<p className="text-sm">Share this link via</p>
+
+						<div className="my-4 flex justify-around">
+							{/* <!--FACEBOOK ICON--> */}
+							<a
+								href="https://www.facebook.com/sharer/sharer.php?u=https://better-quiz.incmak.ml/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-blue-200 fill-[#1877f2] shadow-xl hover:bg-[#1877f2] hover:fill-white hover:shadow-blue-500/50">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
+										<path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
+									</svg>
+								</div>
+							</a>
+							{/* <!--TWITTER ICON--> */}
+							<a
+								href="https://twitter.com/intent/tweet?url=https://better-quiz.incmak.ml/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-blue-200 fill-[#1d9bf0] shadow-xl hover:bg-[#1d9bf0] hover:fill-white hover:shadow-sky-500/50">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
+										<path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path>
+									</svg>
+								</div>
+							</a>
+							{/* <!--INSTAGRAM ICON--> */}
+							<a
+								href="https://www.instagram.com/sharer/sharer.php?u=https://better-quiz.incmak.ml/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-pink-200 fill-[#bc2a8d] shadow-xl hover:bg-[#bc2a8d] hover:fill-white hover:shadow-pink-500/50">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
+										<path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
+										<circle cx="16.806" cy="7.207" r="1.078"></circle>
+										<path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path>
+									</svg>
+								</div>
+							</a>
+
+							{/* <!--WHATSAPP ICON--> */}
+							<a
+								href="https://api.whatsapp.com/send?text=https://better-quiz.incmak.ml/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-green-200 fill-[#25D366] shadow-xl hover:bg-[#25D366] hover:fill-white hover:shadow-green-500/50">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
+										<path
+											fill-rule="evenodd"
+											clip-rule="evenodd"
+											d="M18.403 5.633A8.919 8.919 0 0 0 12.053 3c-4.948 0-8.976 4.027-8.978 8.977 0 1.582.413 3.126 1.198 4.488L3 21.116l4.759-1.249a8.981 8.981 0 0 0 4.29 1.093h.004c4.947 0 8.975-4.027 8.977-8.977a8.926 8.926 0 0 0-2.627-6.35m-6.35 13.812h-.003a7.446 7.446 0 0 1-3.798-1.041l-.272-.162-2.824.741.753-2.753-.177-.282a7.448 7.448 0 0 1-1.141-3.971c.002-4.114 3.349-7.461 7.465-7.461a7.413 7.413 0 0 1 5.275 2.188 7.42 7.42 0 0 1 2.183 5.279c-.002 4.114-3.349 7.462-7.461 7.462m4.093-5.589c-.225-.113-1.327-.655-1.533-.73-.205-.075-.354-.112-.504.112s-.58.729-.711.879-.262.168-.486.056-.947-.349-1.804-1.113c-.667-.595-1.117-1.329-1.248-1.554s-.014-.346.099-.458c.101-.1.224-.262.336-.393.112-.131.149-.224.224-.374s.038-.281-.019-.393c-.056-.113-.505-1.217-.692-1.666-.181-.435-.366-.377-.504-.383a9.65 9.65 0 0 0-.429-.008.826.826 0 0 0-.599.28c-.206.225-.785.767-.785 1.871s.804 2.171.916 2.321c.112.15 1.582 2.415 3.832 3.387.536.231.954.369 1.279.473.537.171 1.026.146 1.413.089.431-.064 1.327-.542 1.514-1.066.187-.524.187-.973.131-1.067-.056-.094-.207-.151-.43-.263"
+										></path>
+									</svg>
+								</div>
+							</a>
+
+							{/* <!--TELEGRAM ICON--> */}
+							<a
+								href="https://t.me/share/url?url=https://better-quiz.incmak.ml/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-sky-200 fill-[#229ED9] shadow-xl hover:bg-[#229ED9] hover:fill-white hover:shadow-sky-500/50">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
+										<path d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434z"></path>
+									</svg>
+								</div>
+							</a>
+						</div>
+
+						<p className="text-sm">Or copy link</p>
+						<div className="mt-4 flex items-center justify-between border-2 border-gray-200 py-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								className="ml-2 fill-gray-500"
+							>
+								<path d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z"></path>
+								<path d="m12 4.929-.707.707 1.414 1.414.707-.707a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0z"></path>
+							</svg>
+
+							<input
+								className="w-full bg-transparent outline-none"
+								type="text"
+								placeholder="link"
+								value="https://better-quiz.incmak.ml"
+							/>
+
+							<button className="mr-2 rounded bg-indigo-500 py-2 px-5 text-sm text-white hover:bg-indigo-600">
+								Copy
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
